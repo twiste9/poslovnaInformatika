9b48374d16fc7af9be4d096f371b5e38e31fbb1d
Stefan Radanovic <radanovicstefan@gmail.com>
2018-07-09 21:51:57 +0200

    init

---
+++ poslovnaBanka/src/main/java/com/app/poslovnaBanka/PoslovnaBankaApplication.java
@@ -0,0 +1,12 @@
+package com.app.poslovnaBanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+}
---
+++ poslovnaBanka/src/test/java/com/app/poslovnaBanka/PoslovnaBankaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.poslovnaBanka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoslovnaBankaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
