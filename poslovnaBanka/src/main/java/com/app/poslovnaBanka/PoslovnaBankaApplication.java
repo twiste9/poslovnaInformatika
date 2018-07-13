e56938959d602a85ba1cae5c26bc8577d70af53b
Mario <Mario@DESKTOP-N11VBT2>
2018-07-13 18:51:35 +0200

    s

--- poslovnaBanka/src/main/java/com/app/poslovnaBanka/PoslovnaBankaApplication.java
+++ poslovnaBanka/src/main/java/com/app/poslovnaBanka/PoslovnaBankaApplication.java
@@ -6,13 +6,15 @@ import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PoslovnaBankaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoslovnaBankaApplication.class, args);
-	}
+}
 	
 	@Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
